d46dbae281e60260447ea04d9a2035f5291a618c
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-04-29 14:23:33 +0200

    Added functions

--- app.py
+++ app.py
@@ -10,114 +10,146 @@ load_dotenv()
 # API KEY
 api_key = os.environ.get('API_KEY')
 
-
 # API Endpoint
-large_mid_cap_url = f'https://apiservice.borsdata.se/v1/instruments?authKey={api_key}'
+nordic_market_companies_url = f'https://apiservice.borsdata.se/v1/instruments?authKey={api_key}'
 
 
-# Fetch all companies listed on Large and Mid Cap
-r = requests.get(large_mid_cap_url)
-d = r.json()
+def fetchNordicMarketCompanies(nordic_market_companies_url):
+    """
+    Function that fetch all companies listed on the Nordic Market
+    """
+    companies = requests.get(nordic_market_companies_url)
+    json_companies = companies.json()
+
+    return json_companies
 
 
-# Array with Large and Mid Cap companies
-large_mid_cap_companies = []
+def fetchLargeMidCap(nordic_market_companies_url):
+    """
+    Function that fetch all companies listed OMX Large and Mid Cap
+        marketId === 1 (Large cap)
+        marketId === 2 (Mid cap)
+    """
+    company = fetchNordicMarketCompanies(nordic_market_companies_url)
 
-for largeCompany in d['instruments']:
-    if largeCompany['marketId'] == 1:
-        large_mid_cap_companies.append(largeCompany)
-
-for midCompany in d['instruments']:
-    if midCompany['marketId'] == 2:
-        large_mid_cap_companies.append(midCompany)
-
-
-# Map each company id with name
-large_mid_cap_companies_dict = {company['insId']: company['name'] for company in large_mid_cap_companies}
-
-
-# Each raw annual report
-large_mid_cap_companies_reports = []
-
-# Fetch annual report for each company
-for company in large_mid_cap_companies:
-    companyId = company['insId']
-    c_url = f'https://apiservice.borsdata.se/v1/instruments/{companyId}/reports/year?authKey={api_key}'
-    response = requests.get(c_url)
-    data = response.json()
-    large_mid_cap_companies_reports.append(data)
-
-
-# Excel header
-df = pd.DataFrame(columns=["År", "Namn", "ROIC", "Börsvärde", "Totala tillgångar", "Totala skulder", "Skuldsättningsgrad"])
-
-
-# Iterate and select the desired key figures from each company
-for p in large_mid_cap_companies_reports:
+    # Array with Large and Mid Cap companies
+    large_mid_cap_companies = []
     
-    instrument_id = p['instrument']
-    instrument_name = large_mid_cap_companies_dict[instrument_id] # map name from arr
+    for largeCompany in company['instruments']:
+        if largeCompany['marketId'] == 1:
+            large_mid_cap_companies.append(largeCompany)
 
-    for report in p['reports']:
+    for midCompany in company['instruments']:
+        if midCompany['marketId'] == 2:
+            large_mid_cap_companies.append(midCompany)
 
-        year = report['year']
-        if year >= 2017 and year <= 2022: # Select reports in range 2017 - 2022
-
-            # ROIC https://github.com/Borsdata-Sweden/API/wiki/KPI-History (KpiId 37)
-            roic_url = f"https://apiservice.borsdata.se/v1/Instruments/{instrument_id}/kpis/37/year/mean/history?authKey={api_key}"
-            roic_response = requests.get(roic_url)
-            roic_data = roic_response.json()
-
-            # Get ROIC for the current year
-            for item in roic_data['values']:
-                if item['y'] == year:
-                    #ROIC
-                    roic = item['v']
-                    break
+    return large_mid_cap_companies
 
 
-            # Market value
-            stock_Price_Average = int(report['stock_Price_Average'])
-            number_Of_Shares = int(report['number_Of_Shares'])
-            market_value = ( stock_Price_Average * number_Of_Shares )
-            
+def mapEachCompany(nordic_market_companies_url):
+    """
+    Function that map each company 'insId' with 'name'
+    """
+    large_mid_cap_companies = fetchLargeMidCap(nordic_market_companies_url)
+    large_mid_cap_companies_dict = {company['insId']: company['name'] for company in large_mid_cap_companies}
 
-            # Total assets
-            total_assets = int(report['total_Assets'])
-
-
-            # Total liabilities
-            current_Liabilities = int(report['current_Liabilities'])
-            non_Current_liabilities = int(report['non_Current_Liabilities'])
-            
-            total_liabilities = ( non_Current_liabilities + current_Liabilities )
+    return large_mid_cap_companies_dict
 
 
-            # Debt ratio
-            total_equity = int(report['total_Equity'])
-            try: 
-                debt_ratio = ( (non_Current_liabilities + current_Liabilities ) / total_equity )
-            
-            except ZeroDivisionError:
-                debt_ratio = 0
+def fetchAnnualReports(nordic_market_companies_url):
+    """
+    Function that fetch all the annual reports for each company
+    """
+    large_mid_cap_companies = fetchLargeMidCap(nordic_market_companies_url)
+
+    # Each raw annual report
+    large_mid_cap_companies_reports = []
+
+    for company in large_mid_cap_companies:
+        companyId = company['insId']
+        c_url = f'https://apiservice.borsdata.se/v1/instruments/{companyId}/reports/year?authKey={api_key}'
+        response = requests.get(c_url)
+        data = response.json()
+        large_mid_cap_companies_reports.append(data)
+
+    return large_mid_cap_companies_reports
 
 
-            # Append the data to the dataframe
-            df = df.append({
-                "År": year,
-                "Namn": instrument_name,
-                "ROIC": roic,
-                "Börsvärde": market_value,
-                "Totala tillgångar": total_assets,
-                "Totala skulder": total_liabilities,
-                "Skuldsättningsgrad": debt_ratio
-            }, ignore_index=True)
+def fetchKeyFigures(nordic_market_companies_url):
+    """
+    Funtion that fetch the selected key figures and creates Excel file
+    """
+    large_mid_cap_companies_dict = mapEachCompany(nordic_market_companies_url)
+    large_mid_cap_companies_reports = fetchAnnualReports(nordic_market_companies_url)
 
 
-# Write the data to the Excel file
-wb = Workbook()
-ws = wb.active
-for r in dataframe_to_rows(df, index=False, header=True):
-    ws.append(r)
-    # Name of the created Excel file
-wb.save("borsdata.xlsx")
+    # Excel header
+    df = pd.DataFrame(columns=["År", "Namn", "ROIC", "Börsvärde", "Totala tillgångar", "Totala skulder", "Skuldsättningsgrad"])
+
+
+    # Iterate and select the desired key figures from each company
+    for company in large_mid_cap_companies_reports:
+        
+        instrument_id = company['instrument']
+        instrument_name = large_mid_cap_companies_dict[instrument_id] # map name from arr
+
+        for report in company['reports']:
+
+            year = report['year']
+            if year >= 2017 and year <= 2022: # Select reports in range 2017 - 2022
+
+                # ROIC https://github.com/Borsdata-Sweden/API/wiki/KPI-History (KpiId 37)
+                roic_url = f"https://apiservice.borsdata.se/v1/Instruments/{instrument_id}/kpis/37/year/mean/history?authKey={api_key}"
+                roic_response = requests.get(roic_url)
+                roic_data = roic_response.json()
+
+                # Get ROIC for the current year
+                for item in roic_data['values']:
+                    if item['y'] == year:
+                        #ROIC
+                        roic = item['v']
+                        break
+
+                # Market value
+                stock_Price_Average = int(report['stock_Price_Average'])
+                number_Of_Shares = int(report['number_Of_Shares'])
+                market_value = ( stock_Price_Average * number_Of_Shares )
+                
+                # Total assets
+                total_assets = int(report['total_Assets'])
+
+                # Total liabilities
+                current_Liabilities = int(report['current_Liabilities'])
+                non_Current_liabilities = int(report['non_Current_Liabilities'])
+                
+                total_liabilities = ( non_Current_liabilities + current_Liabilities )
+
+                # Debt ratio
+                total_equity = int(report['total_Equity'])
+                try: 
+                    debt_ratio = ( (non_Current_liabilities + current_Liabilities ) / total_equity )
+                
+                except ZeroDivisionError:
+                    debt_ratio = 0
+
+                # Append the data to the dataframe
+                df = df.append({
+                    "År": year,
+                    "Namn": instrument_name,
+                    "ROIC": roic,
+                    "Börsvärde": market_value,
+                    "Totala tillgångar": total_assets,
+                    "Totala skulder": total_liabilities,
+                    "Skuldsättningsgrad": debt_ratio
+                }, ignore_index=True)
+
+
+    # Write the data to the Excel file
+    wb = Workbook()
+    ws = wb.active
+    for r in dataframe_to_rows(df, index=False, header=True):
+        ws.append(r)
+        # Name of the created Excel file
+    wb.save("borsdata.xlsx")
+
+fetchKeyFigures(nordic_market_companies_url)
