fea365305969f22656d6df518e86267fa0c7cadc
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-06-05 10:26:32 +0200

    Added error-handling

--- app.py
+++ app.py
@@ -30,16 +30,16 @@ def fetchLargeMidCap(nordic_market_companies_url):
         marketId === 1 (Large cap)
         marketId === 2 (Mid cap)
     """
-    company = fetchNordicMarketCompanies(nordic_market_companies_url)
+    companies = fetchNordicMarketCompanies(nordic_market_companies_url)
 
     # Array with Large and Mid Cap companies
     large_mid_cap_companies = []
     
-    for largeCompany in company['instruments']:
+    for largeCompany in companies['instruments']:
         if largeCompany['marketId'] == 1:
             large_mid_cap_companies.append(largeCompany)
 
-    for midCompany in company['instruments']:
+    for midCompany in companies['instruments']:
         if midCompany['marketId'] == 2:
             large_mid_cap_companies.append(midCompany)
 
@@ -62,13 +62,13 @@ def fetchAnnualReports(nordic_market_companies_url):
     """
     large_mid_cap_companies = fetchLargeMidCap(nordic_market_companies_url)
 
-    # Each raw annual report
+    # Array with each raw report
     large_mid_cap_companies_reports = []
 
     for company in large_mid_cap_companies:
         companyId = company['insId']
-        c_url = f'https://apiservice.borsdata.se/v1/instruments/{companyId}/reports/year?authKey={api_key}'
-        response = requests.get(c_url)
+        company_url = f'https://apiservice.borsdata.se/v1/instruments/{companyId}/reports/year?authKey={api_key}'
+        response = requests.get(company_url)
         data = response.json()
         large_mid_cap_companies_reports.append(data)
 
@@ -83,15 +83,15 @@ def fetchKeyFigures(nordic_market_companies_url):
     large_mid_cap_companies_reports = fetchAnnualReports(nordic_market_companies_url)
 
 
-    # Excel header
-    df = pd.DataFrame(columns=["År", "Namn", "ROIC", "Börsvärde", "Totala tillgångar", "Totala skulder", "Skuldsättningsgrad"])
+    # Excel column headers
+    data_frame = pd.DataFrame(columns=["År", "Namn", "ROIC", "Börsvärde", "Totala tillgångar", "Totala skulder", "Skuldsättningsgrad"])
 
 
     # Iterate and select the desired key figures from each company
     for company in large_mid_cap_companies_reports:
         
         instrument_id = company['instrument']
-        instrument_name = large_mid_cap_companies_dict[instrument_id] # map name from arr
+        instrument_name = large_mid_cap_companies_dict[instrument_id] # map name from array
 
         for report in company['reports']:
 
@@ -102,38 +102,48 @@ def fetchKeyFigures(nordic_market_companies_url):
                 roic_url = f"https://apiservice.borsdata.se/v1/Instruments/{instrument_id}/kpis/37/year/mean/history?authKey={api_key}"
                 roic_response = requests.get(roic_url)
                 roic_data = roic_response.json()
-
                 # Get ROIC for the current year
                 for item in roic_data['values']:
                     if item['y'] == year:
                         #ROIC
-                        roic = item['v']
+                        try:
+                            roic = item['v']
+                        except Exception as e:
+                            print(f'Error {e}')
                         break
 
                 # Market value
-                stock_Price_Average = int(report['stock_Price_Average'])
-                number_Of_Shares = int(report['number_Of_Shares'])
-                market_value = ( stock_Price_Average * number_Of_Shares )
-                
+                try:
+                    stock_Price_Average = int(report['stock_Price_Average'])
+                    number_Of_Shares = int(report['number_Of_Shares'])
+                    market_value = ( stock_Price_Average * number_Of_Shares )
+                except Exception as e:
+                    print(f'Error {e}')
+
                 # Total assets
-                total_assets = int(report['total_Assets'])
+                try:
+                    total_assets = int(report['total_Assets'])
+                except Exception as e:
+                    print(f'Error {e}')
 
                 # Total liabilities
-                current_Liabilities = int(report['current_Liabilities'])
-                non_Current_liabilities = int(report['non_Current_Liabilities'])
-                
-                total_liabilities = ( non_Current_liabilities + current_Liabilities )
+                try:
+                    current_Liabilities = int(report['current_Liabilities'])
+                    non_Current_liabilities = int(report['non_Current_Liabilities'])
+                    total_liabilities = ( non_Current_liabilities + current_Liabilities )
+                except Exception as e:
+                    print(f'Error {e}')
 
                 # Debt ratio
-                total_equity = int(report['total_Equity'])
-                try: 
+                try:
+                    total_equity = int(report['total_Equity'])
                     debt_ratio = ( (non_Current_liabilities + current_Liabilities ) / total_equity )
                 
-                except ZeroDivisionError:
-                    debt_ratio = 0
+                except Exception as e:
+                    print(f'Error {e}')
 
                 # Append the data to the dataframe
-                df = df.append({
+                data_frame = data_frame.append({
                     "År": year,
                     "Namn": instrument_name,
                     "ROIC": roic,
@@ -143,13 +153,16 @@ def fetchKeyFigures(nordic_market_companies_url):
                     "Skuldsättningsgrad": debt_ratio
                 }, ignore_index=True)
 
+    try:
+        # Write the data to the Excel file
+        workbook = Workbook()
+        ws = workbook.active
+        for row in dataframe_to_rows(data_frame, index=False, header=True):
+            ws.append(row)
+            # Name of the created Excel file
+        workbook.save("borsdata.xlsx")
 
-    # Write the data to the Excel file
-    wb = Workbook()
-    ws = wb.active
-    for r in dataframe_to_rows(df, index=False, header=True):
-        ws.append(r)
-        # Name of the created Excel file
-    wb.save("borsdata.xlsx")
+    except Exception as e:
+        print(f'Error {e}')
 
 fetchKeyFigures(nordic_market_companies_url)
